3fe4e403c44f0acfe3d534fcd78e0e9d5482f206
SHIRA BEN ZAKEN <shira441192@gmail.com>
2021-08-20 01:18:42 +0300

    Update index.js
    change function to class
    section 5
    lesson 54

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App =()=> {
+class App extends React.Component {
+    render(){
+        
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err) 
+        );
 
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err) 
-    );
-
-    
-    return <div>
-
-    </div>
-};
+        return <div>
+            Latitude : 
+            Longitude :
+        </div>
+    }
+}
 
 ReactDOM.render(
     <App/>, document.querySelector('#root')

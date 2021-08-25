61009cb90525af425f7057c4483981387208ee94
SHIRA BEN ZAKEN <shira441192@gmail.com>
2021-08-25 17:33:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spiner from './Spiner';
 
 class App extends React.Component {
     state = { lat: null, errorMessage: '' };
@@ -27,7 +28,7 @@ class App extends React.Component {
             );
 
         return (
-            <div>Loading....</div>
+            <Spiner/>
         );
 
     }

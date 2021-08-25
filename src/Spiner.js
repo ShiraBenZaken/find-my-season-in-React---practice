1512a27f0f50159891a0b537bf3984488fbe0918
SHIRA BEN ZAKEN <shira441192@gmail.com>
2021-08-25 17:33:34 +0300

    Create Spiner.js

---
+++ src/Spiner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Spiner =()=> {
+    return (
+        <div class="ui segment">
+            <div class="ui active dimmer">
+                <div class="ui text loader">Loading</div>
+            </div>
+        </div>
+    );
+};
+
+export default Spiner;

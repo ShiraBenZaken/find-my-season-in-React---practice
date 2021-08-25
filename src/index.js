1c4ca0e6102cce06355cfe8ff1aafc25820f1c85
SHIRA BEN ZAKEN <shira441192@gmail.com>
2021-08-25 22:13:18 +0300

    some updates to make the app better

--- src/index.js
+++ src/index.js
@@ -1,39 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
-import Spiner from './Spiner';
+import Spinner from './Spinner';
 
 class App extends React.Component {
     state = { lat: null, errorMessage: '' };
 
-    componentDidMount(){
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({lat: position.coords.latitude}),
-            (err) => this.setState({errorMessage: err.message})
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
         );
     }
 
-    
-    render(){
-
+    renderContent() {
         if (this.state.lat)
             return (
                 <div>
                     <SeasonDisplay lat={this.state.lat} />
-                </div>      
-            );
-        if (this.state.errorMessage )
+                </div>
+            )
+
+        if (this.state.errorMessage)
             return (
                 <div>Error : {this.state.errorMessage}</div>
-            );
+            )
 
         return (
-            <Spiner/>
-        );
+            <Spinner message="Pls accept location request" />
+        )
+    }
 
+    render() {
+        return (
+            <div>
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
 ReactDOM.render(
-    <App/>, document.querySelector('#root')
+    <App />, document.querySelector('#root')
 )
